cfcd8bf503b1ae153e31f12336cf687ca5d1897f
2dimeng <84330408+2dimeng@users.noreply.github.com>
2022-08-24 07:35:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"].split("\n")
-template_id = os.environ["TEMPLATE_ID"].split("\n)
+template_id = os.environ["TEMPLATE_ID"].split("\n")
 
 
 def get_weather_data() :
